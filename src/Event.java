070a9226f0e1386794ec2cf920272b9bd3166ec4
Aiden Cary <ACARY@cub.uca.edu>
2025-02-12 19:50:10 -0600

    Implemented Event class

---
+++ src/Event.java
@@ -0,0 +1,32 @@
+import java.time.LocalDateTime;
+
+public abstract class Event implements Comparable<Event> {
+    protected String name;
+    protected LocalDateTime dateTime;
+
+    // Constructor for the Event class
+    public Event(String name, LocalDateTime dateTime) {
+        this.name = name;
+        this.dateTime = dateTime;
+    }
+
+    public abstract String getName();
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Compares
+    @Override
+    public int compareTo(Event e) {
+        return this.dateTime.compareTo(e.dateTime);
+    }
+
+}
